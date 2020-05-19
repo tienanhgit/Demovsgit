b117b34bcc89561dcc7c724afa642d72ed5135f8
Tien Anh Bui <minhanhtienanh@gmail.com>
2020-05-19 14:45:01 +0700

    Add project files.

---
+++ GitvsBTanh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitvsBTanh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
