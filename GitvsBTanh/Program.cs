ea12d38062088b62f19b15531f9452eee2de7c57
Tien Anh Bui <minhanhtienanh@gmail.com>
2020-05-19 21:30:32 +0700

    desktop da change

--- GitvsBTanh/Program.cs
+++ GitvsBTanh/Program.cs
@@ -6,7 +6,7 @@ namespace GitvsBTanh
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Bui Tien Anh _ Dang o branch btanh_desktop!");
+            Console.WriteLine("Branch desktop cua tao da thay doi");
         }
     }
 }
